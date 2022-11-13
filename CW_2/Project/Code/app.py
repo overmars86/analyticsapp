2a2d70acd6bda2ace545d584624487bd9c76806d
Mohamed <overmars86@gmail.com>
2022-11-13 16:26:20 +0400

    count and conti viz

--- CW_2/Project/Code/app.py
+++ CW_2/Project/Code/app.py
@@ -1,21 +1,47 @@
-from flask import Flask, render_template, request
 import pandas as pd
+from doc import doc_by_country, doc_by_continent, add_cont
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/document/", methods=["GET","POST"])
-def document():
-    uuid = request.form.get("doc_uuid")
-    print(uuid)
-    return render_template("document.html")
+    @app.route("/")
+    def hello_world():
+        return render_template("index.html")
 
-@app.route("/table")
-def table():
-    df = pd.read_json('../data/sample_small.json', lines=True)
-    df = df.iloc[:10, :]
-    tb = df.to_html()
-    return str(tb)
+    #Analyize by document route start
+    @app.route("/document/", methods=["GET","POST"])
+    def document():
+        global uuid
+        uuid = request.form.get("doc_uuid")
+        # if uuid is None:
+        #     return render_template("error.html") #and request.form.clear("doc_uuid")
+        # elif len(uuid) == 45:
+        #     return render_template("document.html") #and request.form.clear("doc_uuid")
+            
+        # else:
+        #     # chart = doc_by_country(str(uuid))
+        #     # print(chart)
+        #     print(uuid)
+        return render_template("document.html")
+            
+
+    @app.route("/viz/")
+    def viz():
+        return doc_by_country(uuid)
+
+    @app.route("/viz_2/")
+    def viz_2():
+        df = add_cont()
+        return doc_by_continent(uuid, df)
+    #Analyize by document route finsih
+
+
+    @app.route("/table")
+    def table():
+        df = pd.read_json('../data/sample_small.json', lines=True)
+        df = df.iloc[:10, :]
+        tb = df.to_html()
+        return str(tb)
+    return app
--- CW_2/Project/Code/doc.py
+++ CW_2/Project/Code/doc.py
@@ -1,14 +1,95 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FC
+from flask import send_file
+import seaborn as sns
+import io, base64
 import numpy as np
 import pandas as pd
 
 
 def doc_by_country(uuid="140222143932-91796b01f94327ee809bd759fd0f6c76"):
+
+    #setting up the chart options
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax = sns.set_style(style = "darkgrid")
+
+    #reading the data and summarize it
     df = pd.read_json('../data/sample_small.json', lines=True)
     df_count = df[['visitor_uuid','visitor_country','env_doc_id']]
     df_count = df_count.loc[df_count['env_doc_id'] == uuid]
     df_count['visitor_uuid'] = df_count['visitor_uuid'].drop_duplicates()
     df_count = df_count[['visitor_uuid','visitor_country']]
-    df_count_gp = df_count.groupby(by='visitor_country').count().sort_values(by='visitor_uuid', 
-        ascending=False).reset_index()
-    df_count_gp.plot(kind='bar', x='visitor_country')
+    df_count_gp = df_count.groupby(by='visitor_country').count().sort_values(
+        by='visitor_uuid', ascending=False).reset_index()
+    # df_count = df_count.rename({"visitor_uuid":"No._of_unique_Vistors"}, axis=1)
+    # df_count = df_count.rename({"visitor_country":"Vistors'_Countries"}, axis=1)
+    
+    #identify x and y
+    x, y = "visitor_country", "visitor_uuid"
+
+    #build the chart
+    sns.barplot(data=df_count_gp, x = x, y = y, hue = x).set(
+        title="Doc UUID is : {uuid}".format(uuid=uuid))
+    print("done...")
+    canvas = FC(fig)
+    img = io.BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img, mimetype="img/png")
+
+
+
+def add_cont():
+    df = pd.read_json('../data/sample_small.json', lines=True)
+    df['Continent'] = 'none'
+    df.loc[df['visitor_country'] == 'BR', 'Continent'] = 'South America'
+    df.loc[df['visitor_country'] == 'US', 'Continent'] = 'Noth America'
+    df.loc[df['visitor_country'] == 'MX', 'Continent'] = 'Noth America'
+    df.loc[df['visitor_country'] == 'CA', 'Continent'] = 'Noth America'
+    df.loc[df['visitor_country'] == 'GB', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'ES', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'PE', 'Continent'] = 'South America'
+    df.loc[df['visitor_country'] == 'IT', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'CO', 'Continent'] = 'South America'
+    df.loc[df['visitor_country'] == 'CL', 'Continent'] = 'South America'
+    df.loc[df['visitor_country'] == 'AU', 'Continent'] = 'Australia & Oceania'
+    df.loc[df['visitor_country'] == 'AR', 'Continent'] = 'South America'
+    df.loc[df['visitor_country'] == 'RU', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'FR', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'PT', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'PL', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'NL', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'SV', 'Continent'] = 'Noth America'
+    df.loc[df['visitor_country'] == 'DE', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'EC', 'Continent'] = 'South America'
+    df.loc[df['visitor_country'] == 'KR', 'Continent'] = 'Asia'
+    df.loc[df['visitor_country'] == 'GT', 'Continent'] = 'Noth America'
+    df.loc[df['visitor_country'] == 'JP', 'Continent'] = 'Asia'
+    df.loc[df['visitor_country'] == 'UA', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'CR', 'Continent'] = 'South America'
+    df.loc[df['visitor_country'] == 'IS', 'Continent'] = 'Europe'
+    df.loc[df['visitor_country'] == 'RS', 'Continent'] = 'Europe'
+    return df
+
+def doc_by_continent(uuid:str("140222143932-91796b01f94327ee809bd759fd0f6c76"), df):
+    fig, ax = plt.subplots(figsize=(8,8))
+    ax = sns.set_style(style = "darkgrid")
+    df_count = df[['visitor_uuid','Continent','env_doc_id']]
+    df_count = df_count.loc[df_count['env_doc_id'] == uuid]
+    df_count['visitor_uuid'] = df_count['visitor_uuid'].drop_duplicates()
+    df_count = df_count[['visitor_uuid','Continent']]
+    df_count_gp = df_count.groupby(by='Continent').count().sort_values(
+        by='visitor_uuid', ascending=False).reset_index()
+    
+    #identify x and y
+    x, y = 'Continent', 'visitor_uuid'
+
+    #build the chart
+    sns.barplot(data=df_count_gp, x = x, y = y, hue = x).set(
+        title="Doc UUID is : {uuid}".format(uuid=uuid))
+    print("done...")
+    canvas = FC(fig)
+    img = io.BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img, mimetype="img/png")
